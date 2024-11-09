6d90c2ede77f8e754f7dceaa089206aa62f49847
Casiopobre <ladysalvitachan@gmail.com>
2024-11-09 17:12:05 +0100

    Falta revisar crear_arco en funciones.c

---
+++ Makefile
@@ -0,0 +1,39 @@
+#opciones de compilacion, muestra todos los warnings (-Wall)
+CC = gcc -Wall
+
+#librerias adicionales, en este caso la matemática
+#LIBS = -lm
+
+#carpeta de las cabeceras (si estan en la actual, ponemos .)
+HEADER_FILES_DIR = .
+
+#opciones de compilacion, indica donde estan los archivos .h
+INCLUDES = -I $(HEADER_FILES_DIR)
+
+#nombre del ejecutable o archivo de salida
+OUTPUT = ejecutable
+
+#ficheros .h. Si hay varios, se precede cada uno con $(HEADER_FILES_DIR)/
+HEADERS = $(HEADER_FILES_DIR)/colors.h $(HEADER_FILES_DIR)/grafo.h $(HEADER_FILES_DIR)/funciones.h
+
+#FUENTES: todos los archivos .c necesarios
+SRCS = main.c grafo.c funciones.c
+
+#ficheros .o: todos los .o con un analogo .c en SRCS
+OBJS = $(SRCS:.c=.o)
+
+#REGLA 1: genera el ejecutable, dependencia de los .o
+$(OUTPUT): $(OBJS)
+	$(CC) -o $(OUTPUT) $(OBJS) $(LIBS)
+
+#REGLA 2: genera los .o cuando es necesario, dependencia de los .c y .h
+%.o: %.c $(HEADERS)
+	$(CC) -c -o $@ $< $(INCLUDES)
+
+# REGLA 3: borra el ejecutable (prerrequisito: clean)
+cleanall: clean
+	rm -f $(OUTPUT)
+
+# REGLA 4: borra los ficheros .o y los de backup (terminan en ~
+clean:
+	rm -f *.o *~
---
+++ colors.h
@@ -0,0 +1,45 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Standard colors
+#define RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+// Bright colors
+#define BRIGHT_BLACK "\033[90m"
+#define BRIGHT_RED "\033[91m"
+#define BRIGHT_GREEN "\033[92m"
+#define BRIGHT_YELLOW "\033[93m"
+#define BRIGHT_BLUE "\033[94m"
+#define BRIGHT_MAGENTA "\033[95m"
+#define BRIGHT_CYAN "\033[96m"
+#define BRIGHT_WHITE "\033[97m"
+
+// Bold colors
+#define BOLD_BLACK "\033[1;30m"
+#define BOLD_RED "\033[1;31m"
+#define BOLD_GREEN "\033[1;32m"
+#define BOLD_YELLOW "\033[1;33m"
+#define BOLD_BLUE "\033[1;34m"
+#define BOLD_MAGENTA "\033[1;35m"
+#define BOLD_CYAN "\033[1;36m"
+#define BOLD_WHITE "\033[1;37m"
+
+// Bright bold colors
+#define BRIGHT_BOLD_BLACK "\033[1;90m"
+#define BRIGHT_BOLD_RED "\033[1;91m"
+#define BRIGHT_BOLD_GREEN "\033[1;92m"
+#define BRIGHT_BOLD_YELLOW "\033[1;93m"
+#define BRIGHT_BOLD_BLUE "\033[1;94m"
+#define BRIGHT_BOLD_MAGENTA "\033[1;95m"
+#define BRIGHT_BOLD_CYAN "\033[1;96m"
+#define BRIGHT_BOLD_WHITE "\033[1;97m"
+
+#endif // COLORS_H
